2405fefe8f258a5d7dfcf8dce2ad9e17c6115b2b
Ayowale <haydexwisdom@gmail.com>
2023-09-18 17:58:45 +0100

    Fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,3 +7,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant privileges to the user on the specified databases
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
