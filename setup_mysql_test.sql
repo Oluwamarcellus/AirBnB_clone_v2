bbd130154f503885cee1a57bd5119966c09cf9e0
Ayowale <haydexwisdom@gmail.com>
2023-09-18 18:04:51 +0100

    Task 4 fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,6 @@
--- Create the database if it doesn't exist
+-- Creates a database a new user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- Grant privileges to the user on the specified databases
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
--- Flush privileges
 FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
