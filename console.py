18c9016c4fd4464914c2d5b624c5488001c9cc70
Ayowale <haydexwisdom@gmail.com>
2023-09-19 17:07:08 +0100

    Refactoring

--- console.py
+++ console.py
@@ -15,7 +15,6 @@ from datetime import datetime
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
-    # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
