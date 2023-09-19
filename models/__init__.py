1d6d92067baa189c5bd0d129c21441debc6afdb5
Noransaber <noransaber685@gmail.com>
2023-09-19 13:38:12 +0300

    Revert "Revert "updates""
    This reverts commit 24daf6cb31bb0d18c740b67d7bcc2a191aeda2f2.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class db_Storage"""
 from models.engine.file_storage import FileStorage
+import os
 
 
-storage = FileStorage()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage  = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    
 storage.reload()
