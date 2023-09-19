24daf6cb31bb0d18c740b67d7bcc2a191aeda2f2
Noransaber <noransaber685@gmail.com>
2023-09-19 13:33:22 +0300

    Revert "updates"
    This reverts commit f041fb36d0c4799c7c8fed33258ff03eee21846f.

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
-class BaseModel(Base):
+class BaseModel:
     """A base class for all hbnb models"""
-    __abstract__ = True  # Indicates that this class should not create a database table on its own
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -22,16 +13,13 @@ class BaseModel(Base):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            for key, value in kwargs:
-                if key != '__class__':
-                    setattr(self, key, value)
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -44,7 +32,6 @@ class BaseModel(Base):
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
-        storage.new(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -54,9 +41,4 @@ class BaseModel(Base):
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
         return dictionary
-    def delete(self):
-        """Delete the current instance from the storage"""
-        storage.delete(self)
-        
