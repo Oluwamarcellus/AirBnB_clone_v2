07c949d2c9c873ff19244cf1f4d4730f8d66861a
Ayowale <haydexwisdom@gmail.com>
2023-09-19 17:49:07 +0100

    Task 10 updated

--- models/amenity.py
+++ models/amenity.py
@@ -6,10 +6,15 @@ from models.base_model import BaseModel
 from models.base_model import Base
 import models
 import os
-
+from models.place import Place, place_amenity
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(
         String(128), nullable=False
         ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    place_amenities = relationship(
+            'Place',
+            secondary=place_amenity,
+            back_populates="amenities"
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
