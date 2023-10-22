a0907753b73ce189534217921204257e4b486575
Ayowale <haydexwisdom@gmail.com>
2023-10-22 14:51:21 +0100

    error fixed

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
+def cities_states():
     """Displays an HTML page with a list of all states and related cities"""
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
